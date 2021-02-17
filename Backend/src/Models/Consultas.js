60d4d70bc34d917e62573c5f6504b6d912d9f91a
Jefferson Geovanny <jeffgeomp@gmail.com>
2021-02-17 16:53:14 -0600

    Add: Campo de Imagen en Respuestas

--- Backend/src/Models/Consultas.js
+++ Backend/src/Models/Consultas.js
@@ -4,25 +4,25 @@ class Consultas{
     }
 
     PlatillosPorMenu(Tipo_Menu){
-        return 'SELECT P.id_platillo, P.nombre, P.precio, P.descripcion ' +
+        return 'SELECT P.id_platillo, P.nombre, P.precio, P.descripcion, P.imagen ' +
                 'FROM platillo P  ' +
                 'INNER JOIN menu M ON P.id_menu = M.id_menu  WHERE M.menu = \''+Tipo_Menu+'\''
     }
 
     Platillos(){
-        return 'SELECT P.id_platillo, P.nombre, P.precio, P.descripcion ' +
+        return 'SELECT P.id_platillo, P.nombre, P.precio, P.descripcion, P.imagen ' +
                 'FROM platillo P'
     }
 
     BusquedaPorMenu(Tipo_Menu, Palabra){
-        return 'SELECT P.id_platillo, P.nombre, P.descripcion, P.precio ' +
+        return 'SELECT P.id_platillo, P.nombre, P.descripcion, P.precio, P.imagen ' +
                 'FROM platillo P ' +
                 'INNER JOIN menu M ON P.id_menu = M.id_menu ' +
                 'WHERE M.menu = \'' + Tipo_Menu + '\' AND P.nombre LIKE \'' + Palabra + '%\''
     }
 
     BusquedaGeneral(Palabra){
-        return 'SELECT P.id_platillo, P.nombre, P.descripcion, P.precio ' +
+        return 'SELECT P.id_platillo, P.nombre, P.descripcion, P.precio, P.imagen ' +
                 'FROM platillo P ' +
                 'WHERE P.nombre LIKE \'' + Palabra + '%\''
     }
