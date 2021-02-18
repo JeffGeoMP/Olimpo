1f69b96cd4786f17d2a4c6e6b769839454ad33ea
Hayrton <2172182021503@ingenieria.usac.edu.gt>
2021-02-18 00:17:02 -0600

    201313875 - consulta para menu del dia

--- AdministradorBD/Query.sql
+++ AdministradorBD/Query.sql
@@ -11,3 +11,7 @@ where M.id_Menu = P.id_Menu
 and M.menu = 'Almuerzo' and P.Nombre Like 'P_%';
 
 -- Consulta Menu del dia
+select P.nombre, P.descripcion, P.precio
+from menu M, platillo P
+where M.id_Menu = P.id_Menu
+and M.menu = 'del dia';
