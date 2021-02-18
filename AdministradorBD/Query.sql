f9df94dfe654d6d8c581ec202eb81bb372c0467f
Alejandro Marín <cheealejoomariin@gmail.com>
2021-02-18 00:45:52 -0600

    201602855- Corrección de insert

--- AdministradorBD/Query.sql
+++ AdministradorBD/Query.sql
@@ -2,16 +2,16 @@
 select P.nombre, P.descripcion, P.precio
 from menu M, platillo P
 where M.id_Menu = P.id_Menu
-and M.menu = 'Almuerzo';
+and lower(M.menu) = 'Almuerzo';
 
 -- Consulta buscador
 select P.nombre, P.descripcion, P.precio
 from menu M, platillo P
 where M.id_Menu = P.id_Menu
-and M.menu = 'Almuerzo' and P.Nombre Like 'P_%';
+and lower(M.menu) = 'almuerzo' and lower(P.Nombre) Like 'P_%';
 
 -- Consulta Menu del dia
 select P.nombre, P.descripcion, P.precio
 from menu M, platillo P
 where M.id_Menu = P.id_Menu
-and M.menu = 'del dia';
+and lower(M.menu) = 'del dia';
