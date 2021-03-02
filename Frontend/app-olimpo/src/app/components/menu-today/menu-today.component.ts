9f6b2ac0ed6785f1bdaa85947e5b283a99716260
Manases <2791808530307@ingenieria.usac.edu.gt>
2021-03-01 21:18:37 -0600

    201700529 - Diseño

--- Frontend/app-olimpo/src/app/components/menu-today/menu-today.component.ts
+++ Frontend/app-olimpo/src/app/components/menu-today/menu-today.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-menu-today',
-  templateUrl: './menu-today.component.html'
+  templateUrl: './menu-today.component.html',
+  styleUrls: ['./menu-today.component.css']
 })
 export class MenuTodayComponent implements OnInit {
 
