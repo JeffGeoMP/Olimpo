7ccc40c7e57556bb37f131199ea0cb83d55f3242
Hayrton <2172182021503@ingenieria.usac.edu.gt>
2021-03-03 19:05:19 -0600

    201313875 - correccion de codigo

--- Frontend/app-olimpo/src/app/components/menus/menus.component.ts
+++ Frontend/app-olimpo/src/app/components/menus/menus.component.ts
@@ -19,31 +19,22 @@ export class MenusComponent implements OnInit {
     //desayunos
     this._productoService.productoxMenu('desayuno').subscribe((res:Producto[])=>{
       this.productosdesayuno = res;
-      //console.log('imprimiendo desayuno');
-      //console.log(this.productosdesayuno);
     })
 
     //Almuerzo
     this._productoService.productoxMenu('almuerzo').subscribe((res:Producto[])=>{
       this.productosalmuerzo = res;
-      //console.log('imprimiendo desayuno');
-      //console.log(this.productosdesayuno);
     })
     //Infantil
     this._productoService.productoxMenu('infantil').subscribe((res:Producto[])=>{
       this.productosinfantil = res;
-      //console.log('imprimiendo desayuno');
-      //console.log(this.productosdesayuno);
     })
     //Refaccion
     this._productoService.productoxMenu('refaccion').subscribe((res:Producto[])=>{
       this.productosrefaccion = res;
-      //console.log('imprimiendo desayuno');
-      //console.log(this.productosrefaccion);
     })
-    //Extras
-    //Refaccion
 
+    //todos los productos
     /*this._productoService.getProductos().subscribe((res: Producto[]) =>{
       this.productos = res;
       console.log(this.productos);
