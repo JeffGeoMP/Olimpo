52089a44b67d37c87d268394809492e625898a8f
Miguel Angel <miguelksangel@gmail.com>
2021-03-13 18:38:33 -0600

    201700543 - creacion de empleado

--- Frontend/app-olimpo/src/app/servicios/empleado/crud.service.ts
+++ Frontend/app-olimpo/src/app/servicios/empleado/crud.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CrudService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  headers: HttpHeaders = new HttpHeaders({
+    "Content-Type": "application/json"
+  })
+
+  dir = 'http://localhost:3000/';
+
+  getEmpleado(id){
+    let url = `${this.dir}/getEmpleado`;
+    return this.http.get(url, id);
+  }
+
+  createEmpleado(data){
+    let url = `${this.dir}/createEmpleado`;
+    return this.http.post(url, data);
+  }
+
+  updateEmpleado(data){
+    let url = `${this.dir}/updateEmpleado`;
+    return this.http.put(url, data);
+  }
+
+  deleteEmpleado(id){
+    let url = `${this.dir}/deleteEmpleado`;
+    return this.http.delete(url, id);
+  }
+
 }
+
+export interface Empleado{
+  id_persona,
+  Nombre,
+  Apellido,
+  Telefono,
+  Correo,
+  contraseña,
+  Direccion,
+  Tipo_Persona,
+};
